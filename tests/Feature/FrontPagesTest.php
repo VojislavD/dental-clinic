4f25f972f88a9d3fe37c4e8466916dd2bb37c398
vojislavd <vojislavdragicevic17@gmail.com>
2022-03-21 17:19:03 +0100

    Test fron pages

---
+++ tests/Feature/FrontPagesTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FrontPagesTest extends TestCase
+{
+    /** @test */
+    public function test_welcome_page()
+    {
+        $response = $this->get(route('welcome'));
+
+        $response->assertOk()
+            ->assertSee(__('Achive Your Desired'));
+    }
+
+    /** @test */
+    public function test_about_page()
+    {
+        $response = $this->get(route('about'));
+
+        $response->assertOk()
+            ->assertSee(__('About Us'));
+    }
+
+    /** @test */
+    public function test_services_page()
+    {
+        $response = $this->get(route('services'));
+
+        $response->assertOk()
+            ->assertSee(__('Our Services'));
+    }
+
+    /** @test */
+    public function test_gallery_page()
+    {
+        $response = $this->get(route('gallery'));
+
+        $response->assertOk()
+            ->assertSee(__('Our Work'));
+    }
+
+    /** @test */
+    public function test_team_page()
+    {
+        $response = $this->get(route('team'));
+
+        $response->assertOk()
+            ->assertSee(__('Our Team'));
+    }
+
+    /** @test */
+    public function test_contact_page()
+    {
+        $response = $this->get(route('contact'));
+
+        $response->assertOk()
+            ->assertSee(__('Contact Us'));
+    }
+}
